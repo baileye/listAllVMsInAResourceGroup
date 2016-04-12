dcd8d334e8bf5ed6b03b4118b8da6ab14c659450
Eoin Bailey <baileye@gmail.com>
2016-04-12 17:47:00 +0100

    Update list.ps1

--- list.ps1
+++ list.ps1
@@ -1,21 +1,30 @@
-$coresPerVM = 16;
-
+$coresPerVm = 16; #hard coded, but should query the VM to get the info
 $count = 0;
 
-$AzureResourceGroup1 = 'rdp';
+$resourceGroups = @("rdp", "rdp")
 
 Write-Output "Azure Hybrid Render Virtual Machines:";
-Get-AzureRmVM -ResourceGroupName $AzureResourceGroup1 | ForEach-Object {  
-    $count++;
-    Write-Host -NoNewline "## VM " $count;
-    Write-Output ""
-    Start-Sleep -m 40
+
+ForEach ($resourceGroup in $resourceGroups) {
+    Get-AzureRmVM -ResourceGroupName $resourceGroup | ForEach-Object {  
+        $count++;
+        Write-Host -NoNewline "## VM " $count;
+        Write-Output ""
+        Start-Sleep -m 40
+    }
 }
+
 Write-Output ""
 Write-Output "################"
+Write-Output ""
+Write-Output ""
 Write-Output ""
 
 Write-Host -NoNewline "### Number of Virtual Machines: " $count;
 Write-Output ""
-$cores = $count*$coresPerVM;
+$cores = $count*$coresPerVm
 Write-Host -NoNewline "### Number of Render Cores:     " $cores;
+Write-Output ""
+Write-Output ""
+Write-Output ""
+Write-Output ""
